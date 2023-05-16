bb810e086b5a4f7f4cd93c9db7570a1ebb8f0195
Ram Narayan <shukla.narayanram@gmail.com>
2023-05-16 17:30:01 +0530

    ADD

--- middlewares/grantAccessTo.js
+++ middlewares/grantAccessTo.js
@@ -1,9 +1,11 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = 'newtonSchool';
+const JWT_SECRET = "newtonSchool";
 
 /*
-Write a middleware function that checks if the user's role matches with the one given passed in the middleware as an array of roles. The token should be passed in the authorization header, the middle should return a 403 status code if the user's role is not present in the roles array and a 401 status code if the token is invalid or missing.
+Write a middleware function that checks if the user's role matches with the one given passed in the middleware as an array of roles. 
+The token should be passed in the authorization header, the middle should return a 403 status code if the user's role is not present in 
+the roles array and a 401 status code if the token is invalid or missing.
 
 Possible Cases: 
 1. Token is missing : { message: 'Authentication failed: Missing token.', status: "Error"}
@@ -11,7 +13,7 @@ Possible Cases:
 3. Role Doesn't Match: { message: 'Access Denied', status: "Error" }
 */
 
-function grantAccessTo(roles) { return function (req, res, next) {
+function grantAccessTo(roles) {
   return function (req, res, next) {
     //Write your code here;
     const token = req.headers.authorization;
@@ -32,12 +34,15 @@ function grantAccessTo(roles) { return function (req, res, next) {
           .json({ message: "Access Denied", status: "Error" });
       }
       next();
-    
     } catch (err) {
-      return res.status(401).json({ message: 'Authentication failed: Invalid token.', status: "Error" });
+      return res
+        .status(401)
+        .json({
+          message: "Authentication failed: Invalid token.",
+          status: "Error",
+        });
     }
-  }
+  };
 }
 
 module.exports = grantAccessTo;
-
